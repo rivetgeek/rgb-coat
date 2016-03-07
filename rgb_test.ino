844f5e00a33ba91989cc9617fd35173c0a72492f
rivetgeek <rivetgeek@gmail.com>
2016-03-07 15:11:58 -0800

    janky fix for duplicate pixels

--- rgb_test.ino
+++ rgb_test.ino
@@ -2,55 +2,58 @@
 #define NUM_LEDS 96
 #define DATA_PIN 3
 #define CLOCK_PIN 4
-#define NUM_ACTIVE 20
+#define NUM_ACTIVE 20 //This changes how many pixels are on at any given time
 
 CRGBArray<NUM_LEDS> leds;
 
-//array of active LEDS
+//array of currently active LEDS
 int RunningPins[NUM_ACTIVE];
 
-//array of last setting for active LEDS
+//array of last color index for active LEDS
 int LastColorIndex[NUM_ACTIVE];
+
 int unsigned long lastPinTime;
+
+//Color palette that will be sewpt through with cos function.
 CRGBPalette16 currentPalette = RainbowColors_p;
 TBlendType    currentBlending = LINEARBLEND;
 
 void setup() {
-  
+
   delay( 3000 ); // power-up safety delay
   FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
-SetupPalette();
+  SetupPalette();
 }
 
 void loop() {
 
-    //set the color for each pin in the running array
-   for (int i=0;i<NUM_ACTIVE;i++){
-    
-    if ( millis() - lastPinTime > 3000){
-      while 
-  RunningPins[i] = random(0,NUM_LEDS);
-  lastPinTime = millis();
-}
+  //Loop through all the pixels
+  for (int i = 0; i < NUM_ACTIVE; i++) {
 
-      //if cycle is complete, reset the color index and blank the pin
-      if (LastColorIndex[i] >255){
-          leds[RunningPins[i]] = CRGB::Black;
-          //if time to start new pin
-//*****
-//*****
-          RunningPins[i] = NULL;
-          LastColorIndex[i]=0;
-   
-        }
+    //Is it time to turn on a new pixel?
+    if ( millis() - lastPinTime > 3000) {
+      if (RunningPins[i] == 999) {
+        //add a random pixel to the Running Pins array.
+        RunningPins[i] = random(0, NUM_LEDS);
+        //reset the counter
+        lastPinTime = millis();
+      }
+    }
 
+    //if cycle is complete, reset the color index and blank the pin
+    if (LastColorIndex[i] > 255) {
+      leds[RunningPins[i]] = CRGB::Black;
+      RunningPins[i] = 999;
+      LastColorIndex[i] = 0;
+    }
+    //Change the pixels to the respective color index based on their place in the cycle.
     leds[RunningPins[i]] = ColorFromPalette(currentPalette, LastColorIndex[i], 64, currentBlending);
-    LastColorIndex[i]+=1;
-   }
-   
+    LastColorIndex[i] += 1;
+  }
+
   FastLED.delay(60);
 
-  
+
 
 }
 
@@ -58,20 +61,17 @@ void loop() {
 
 void SetupPalette()
 {
-    CRGB AliceBlue = CRGB::AliceBlue;
-    CRGB Amethyst  = CRGB::Amethyst;
-    CRGB Aqua = CRGB::Aqua;
-    CRGB Aquamarine = CRGB::Aquamarine;
-    CRGB black  = CRGB::Black;
-    
-    currentPalette = CRGBPalette16(
-                                   black, AliceBlue,  Amethyst,  Aqua,
-                                   AliceBlue,  Amethyst,  Aqua,  Aquamarine,
-                                   AliceBlue,  Amethyst,  Aqua,  Aquamarine,
-                                   AliceBlue,  Amethyst,  Aqua,  black
-                                   );
+  CRGB AliceBlue = CRGB::AliceBlue;
+  CRGB Amethyst  = CRGB::Amethyst;
+  CRGB Aqua = CRGB::Aqua;
+  CRGB Aquamarine = CRGB::Aquamarine;
+  CRGB black  = CRGB::Black;
+
+  currentPalette = CRGBPalette16(
+                     black, AliceBlue,  Amethyst,  Aqua,
+                     AliceBlue,  Amethyst,  Aqua,  Aquamarine,
+                     AliceBlue,  Amethyst,  Aqua,  Aquamarine,
+                     AliceBlue,  Amethyst,  Aqua,  black
+                   );
 }
-
-
-
 
